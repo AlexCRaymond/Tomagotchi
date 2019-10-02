9866f25bce9f6a5775ae342b7a8b5a119e6f4dc1
Alex Raymond <alexraymond@Alexs-MacBook-Air.local>
2019-10-02 15:04:45 -0700

    working timers! Still need to fix but hunger is working

--- js/app.js
+++ js/app.js
@@ -4,21 +4,16 @@ class Tomagotchi {
     constructor(tomagotchisName) {
         this.name = tomagotchisName;
         this.age = 0;
-        this.hunger = randomNumber(0,3)
-        this.sleep = randomNumber(0,2)
+        this.hunger = 0;
+        this.sleep = 0;
         this.boredom = 0
-        
     }
     getAge() {
         this.age++
         const $age = $(`#age`)
         $age.text(`Age: ${this.age}`)
     }
-    getHungry() {
-        this.hunger++
-        const $hunger = $(`#hunger`)
-        $hunger.text(`Hunger: ${this.hunger}`)
-    }
+   
     getBored() { 
         this.boredom++
         const $boredom = $(`#boredom`)
@@ -47,7 +42,6 @@ class Tomagotchi {
     
 }
 let start = prompt("Name your Tomagotchi!")
-console.log(start)
 const petName = new Tomagotchi(start);
 const $tama=$('#name')
 $tama.text(petName.name)
@@ -63,9 +57,16 @@ const game = {
             this.tomagotchisUpdate()
         }, 1000)
     },
+    setHunger() {
+        setInterval(() => {
+            const $hunger = $('#hunger')
+            this.tomagotchis[0].hunger++
+            $hunger.text(`Hunger: ${this.tomagotchis[0].hunger}`)
+        }, randomNumber(5000,9000))
+    },
     tomagotchisUpdate() {
         for(let i = 0; i < this.tomagotchis.length; i++) {
-            this.tomagotchis[i].getHungry()
+            //this.tomagotchis[i].getHungry()
             this.tomagotchis[i].getSleep()
             this.tomagotchis[i].getBored()
             this.tomagotchis[i].getAge()
@@ -98,6 +99,7 @@ $('.buttons').on('click',(e) => {
 const morty = new Tomagotchi('Morty', 3);
 game.tomagotchis.push(morty)
 game.setTimer()
+game.setHunger()
 
 
 
