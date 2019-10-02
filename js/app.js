0280ab3d47df24f6cfc49a70eb94dda056abcece
Alex Raymond <alexraymond@Alexs-MacBook-Air.local>
2019-10-02 11:05:55 -0700

    timers working no event listeners yet

--- js/app.js
+++ js/app.js
@@ -1,11 +1,60 @@
 class Tomagotchi {
-    constructor(tomagotchisName, tomagotchisaAge) {
+    constructor(tomagotchisName, tomagotchisAge) {
         this.name = tomagotchisName;
-        this.age = tomagotchisaAge;
+        this.age = tomagotchisAge;
+        this.hunger = 0,
+        this.sleep = 0,
+        this.boredom = 0
+    }
+    getHungry() {
+        this.hunger++
+        const $hunger = $(`#hunger`)
+        $hunger.text(`Hunger: ${this.hunger}`)
+    }
+    getBored() { 
+        this.boredom++
+        const $boredom = $(`#boredom`)
+        $boredom.text(`Boredom: ${this.boredom}`)  
+    }
+    getSleep() { 
+        this.sleep++
+        const $sleep = $(`#sleep`)
+        $sleep.text(`Sleepiness: ${this.sleep}`)
+    }
 
+}
+const game = { 
+    tomagotchis: [],
+    time: 0,
+    setTimer() {
+        setInterval(() => {
+            const $clock = $('#clock')
+            this.time++
+            $clock.text(`Clock: ${this.time}s`)
+            this.tomagotchisUpdate()
+        }, 1000)
+    },
+    tomagotchisUpdate() {
+        for(let i = 0; i < this.tomagotchis.length; i++) {
+            this.tomagotchis[i].getHungry()
+            this.tomagotchis[i].getSleep()
+            this.tomagotchis[i].getBored()
+        }
     }
 }
+
+const rick = new Tomagotchi('Rick', 55);
 const morty = new Tomagotchi('Morty', 3);
+game.tomagotchis.push(morty)
+game.tomagotchis.push(rick)
+game.setTimer()
+
+
+
+
+
+
+
 
 
 
